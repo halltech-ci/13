40419588f6a8b4f79af8f97d03f85e42081eb226
halltech-ci <maurice.atche@halltech-africa.com>
2020-12-11 23:48:28 +0000

    Adding purchase_request_custom and sale_custom

--- sale_custom/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ sale_custom/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "purchase_request_custom",
+    'name': "sale_custom",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -16,12 +16,11 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Purchase Management',
+    'category': 'Sale',
     'version': '13.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['purchase_request',
-               'sale_custom'],
+    'depends': ['sale_management'],
 
     # always loaded
     'data': [
