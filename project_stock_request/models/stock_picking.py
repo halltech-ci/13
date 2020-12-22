901002fe71294ca1ee0a4e846d4409ac1b6d5254
halltech-ci <maurice.atche@halltech-africa.com>
2020-12-22 12:11:50 +0000

    Deleting project_custom, project_stock_request, stock_request_project

--- project_stock_request/models/stock_picking.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class StockPicking(models.Model):
-    _inherit = "stock.picking"
-    
-    request_id = fields.Many2one('stock.move.request', string="Transfert")
--- stock_request_project/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import project_task
-from . import stock_rule
-from . import stock_request
-from . import stock_request_order
