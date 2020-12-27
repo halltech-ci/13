b48252992a20ff9ca822ab9a0ede00b659e881ce
halltech-ci <maurice.atche@halltech-africa.com>
2020-12-27 22:17:21 +0000

    Adding hr dependency in hta_custom_expense

--- hta_custom_expense/models/hr_employee.py
+++ hta_custom_expense/models/hr_employee.py
@@ -5,7 +5,8 @@ from odoo import models, fields, api
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
     
-    expense_cost = fields.Monetary("Expense Cost", compute='_compute_expense_cost', store=True,)
+    expense_cost = fields.Monetary("Expense Cost", compute='_compute_expense_cost', store=True, currency_field='currency_id')
+    currency_id = fields.Many2one('res.currency', string='Currency', readonly=True, states={'draft': [('readonly', False)], 'refused': [('readonly', False)]}, default=lambda self: self.env.company.currency_id) 
     
     def _compute_expense_cost(self):
         for record in self:
@@ -16,7 +17,3 @@ class HrEmployee(models.Model):
                     cost += expense.total_amount
             record.expense_cost = cost
             
-class EmployeePublic(models.Model):
-    _inherit = 'hr.employee.public'
-    
-    expense_cost = fields.Monetary("Expense Cost", store=True)
