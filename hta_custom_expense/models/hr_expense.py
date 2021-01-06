0da113f4e9f4599855d16b9beae212945483cf2d
halltech-ci <maurice.atche@halltech-africa.com>
2021-01-06 00:20:38 +0000

    Overiding get_expense_account_destination in hr_expense

--- hta_custom_expense/models/hr_expense.py
+++ hta_custom_expense/models/hr_expense.py
@@ -63,3 +63,18 @@ class HrExpense(models.Model):
         }
         return move_values
     
+    
+    def _get_expense_account_destination(self):
+        self.ensure_one()
+        account_dest = self.env['account.account']
+        if self.payment_mode == 'company_account' or self.payment_mode == 'employee':
+            if not self.sheet_id.bank_journal_id.default_credit_account_id:
+                raise UserError(_("No credit account found for the %s journal, please configure one.") % (self.sheet_id.bank_journal_id.name))
+            account_dest = self.sheet_id.bank_journal_id.default_credit_account_id.id
+        else:
+            if not self.employee_id.sudo().address_home_id:
+                raise UserError(_("No Home Address found for the employee %s, please configure one.") % (self.employee_id.name))
+            partner = self.employee_id.sudo().address_home_id.with_context(force_company=self.company_id.id)
+            account_dest = partner.property_account_payable_id.id or partner.parent_id.property_account_payable_id.id
+        return account_dest
+    
