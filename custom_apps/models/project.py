42296bb8158c63655fa9077e1412ab1d69504efe
JONAS KACOU <jonasgnangbi@gmail.com>
2021-01-11 08:41:20 +0000

    Modification de sale_order_ids dans le projet

--- custom_apps/models/project.py
+++ custom_apps/models/project.py
@@ -20,11 +20,11 @@ from odoo import models, fields, api, _
 class Project(models.Model):
     _inherit = "project.project"
     
-    sale_order_ids = fields.One2many("sale.order", "project_id", string="Sale Orders")
+    sale_order_ids = fields.One2many(comodel_name = "sale.order", inverse_name = "analytic_account_id", string="Sale Orders")
     #purchase_order_lines = fields.One2many("purchase.order.line", "project_id", string="Purchase Orders")
     
     project_code_sequence_id = fields.Many2one(comodel_name='ir.sequence', string='Code Sequence Id', ondelete='cascade')
-    project_code = fields.Char(string='Code Projet', required=True, readonly=True, copy=False, default= lambda self: _('New'))
+    #project_code = fields.Char(string='Code Projet', required=True, readonly=True, copy=False, default= lambda self: _('New'))
     
     #Create method to compute project_code from id like P%year/0000 + id
     @api.model
